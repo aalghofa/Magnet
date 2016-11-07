0588f5ac37768bb0921797986db54f6b1aa299bf
aalghofa <aalghofa@kent.edu>
2016-11-07 00:14:41 -0500

    aws db settings

--- settings.py
+++ settings.py
@@ -3,15 +3,27 @@ import os
 SECRET_KEY = "b'\xf9\x07K\xc2\x11\x9ee\xd4\x8a\xe3\x91\xd9\x88\xca\xba\x1d\xb2\x9d\xb5\xdb\x82;\xd6\xd4'"
 DEBUG = True
 
-DB_USERNAME = 'root'
-DB_PASSWORD = 'AGmysql--'
-############### Mysql PASSWORD ##################
+# DB_USERNAME = 'root'
+# DB_PASSWORD = 'AGmysql--'
 
-BLOG_DATABASE_NAME = 'magnet'
-DB_HOST = os.getenv('IP','127.0.0.1')
-DB_URI = "mysql+pymysql://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST, BLOG_DATABASE_NAME)
-SQLALCHEMY_DATABASE_URI = DB_URI
-SQLALCHEMY_TRACK_MODIFICATIONS = True
+# BLOG_DATABASE_NAME = 'magnet'
+# DB_HOST = os.getenv('IP','127.0.0.1')
+# DB_URI = "mysql+pymysql://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST, BLOG_DATABASE_NAME)
+# SQLALCHEMY_DATABASE_URI = DB_URI
+# SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
 #EMAIL SETTINGS
+
+
+#for aws db settings 
+# format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
+QLALCHEMY_DATABASE_URI = 'mysql+pymysql://magnethms:Magnet123--@magnethms.cqrzi7laicv7.us-west-2.rds.amazonaws.com:3306.amazonaws.com:3306/magnet'
+
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+SQLALCHEMY_POOL_RECYCLE = 3600
+
+WTF_CSRF_ENABLED = True
+
+# magnethms.cqrzi7laicv7.us-west-2.rds.amazonaws.com:3306
